27914f10088d474730f01ffbf3f591d5b710fd62
berkinozturk <beerkinozturk@gmail.com>
2024-02-02 17:48:58 +0300

    Caching implemented with Redis to increase performance

--- src/main/java/com/berkinozturk/event/service/EventService.java
+++ src/main/java/com/berkinozturk/event/service/EventService.java
@@ -3,12 +3,16 @@ package com.berkinozturk.event.service;
 import com.berkinozturk.event.entity.EventEntity;
 import com.berkinozturk.event.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@EnableCaching
 public class EventService {
 
     private final EventRepository eventRepository;
@@ -16,15 +20,15 @@ public class EventService {
     public String saveEvent(EventEntity eventEntity) {
         return eventRepository.save(eventEntity).getId();
     }
-
+    @Cacheable(value = "events", key = "#id")
     public EventEntity findById(String id) {
         return eventRepository.findById(id).orElse(null);
     }
-
+    @CacheEvict(value = "events", key = "#id")
     public void deleteEvent(String id) {
         eventRepository.deleteById(id);
     }
-
+    @Cacheable(value = "allEvents")
     public List<EventEntity> findAllEvents() {
         return eventRepository.findAll();
     }
