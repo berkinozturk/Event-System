fd9ac4e909aac8215406fb52ed0d5f780bfccbac
berkinozturk <beerkinozturk@gmail.com>
2024-02-11 00:16:35 +0300

    Caching implemented for Users

--- src/main/java/com/berkinozturk/event/service/UserService.java
+++ src/main/java/com/berkinozturk/event/service/UserService.java
@@ -7,6 +7,9 @@ import com.berkinozturk.event.exception.UnauthorizedException;
 import com.berkinozturk.event.repository.EventRepository;
 import com.berkinozturk.event.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +20,8 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userEntityRepository;
-    private final EventRepository eventRepository;
 
+    @CachePut(value = "users", key = "#user.id")
     public UserEntity createUser(String username, String password, String email, String fullName, RoleType role) {
         UserEntity user = UserEntity.builder()
                 .username(username)
@@ -31,13 +34,16 @@ public class UserService {
         return userEntityRepository.save(user);
     }
 
+    @Cacheable(value = "users", key = "#userId")
     public Optional<UserEntity> findUserById(String userId) {
         return userEntityRepository.findById(userId);
     }
 
+    @Cacheable(value = "users", key = "#username")
     public Optional<UserEntity> findUserByUsername(String username) {
         return userEntityRepository.findByUsername(username);
     }
+
 
     public void addRoleToUser(String userId, RoleType role) {
         UserEntity user = userEntityRepository.findById(userId)
@@ -47,6 +53,7 @@ public class UserService {
         userEntityRepository.save(user);
     }
 
+    @CachePut(value = "users", key = "#userId")
     public UserEntity updateProfile(String userId, String fullName, String email) {
         UserEntity user = userEntityRepository.findById(userId)
                 .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + userId));
@@ -55,6 +62,7 @@ public class UserService {
         user.setEmail(email);
         return userEntityRepository.save(user);
     }
+    @CacheEvict(value = "users", key = "#userId")
     public void deleteUser(String userId) {
         userEntityRepository.findById(userId)
                 .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + userId));
