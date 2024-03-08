a63ca6797750b081415c9e739c36a29f3c38b0b4
berkinozturk <beerkinozturk@gmail.com>
2024-03-08 18:59:08 +0300

    Update EventService.java

--- src/main/java/com/berkinozturk/event/service/EventService.java
+++ src/main/java/com/berkinozturk/event/service/EventService.java
@@ -20,8 +20,9 @@ public class EventService {
     private final EventRepository eventRepository;
     private final UserRepository userRepository;
 
-    // I am not sure but does caching even work here?
-    // How do you know that the value is fetched from Redis if it is in Redis before you fetch it from the database?
+    // The caching is working here, because I controlled it with Redis CLI. I can see the values in the cache.
+    // When I typed KEYS * in the Redis CLI, I can see the keys of the cache. And when I typed GET <key>, I can see the values of the cache.
+    // After the TTL duration, the keys are deleted from the cache. So, the caching is working here.
     @Cacheable(value = "events", key = "#id")
     public EventEntity findById(String id) {
         return eventRepository.findById(id).orElse(null);
