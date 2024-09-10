96fdb24496af3b65315c4a3bbcac64518bafc8a8
berkinozturk <beerkinozturk@gmail.com>
2024-09-11 00:27:05 +0200

    Improved authentication error catching mechanism

--- src/main/java/com/berkinozturk/event/service/AuthenticationService.java
+++ src/main/java/com/berkinozturk/event/service/AuthenticationService.java
@@ -11,7 +11,9 @@ import com.berkinozturk.event.mapper.RegisterRequestToUserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -38,17 +40,25 @@ public class AuthenticationService {
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
-        } catch (BadCredentialsException ex) {
-            throw new RuntimeException("Invalid credentials: Bad credentials", ex);
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+
+            UserEntity user = userRepository.findByUsername(request.getUsername())
+                    .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+            String jwtToken = jwtService.generateToken(user);
+            return AuthenticationResponse.builder()
+                    .token(jwtToken)
+                    .build();
+
+        } catch (AuthenticationException ex) {
+            throw new BadCredentialsException("Authentication failed: Invalid credentials", ex);
+
+        } catch (Exception ex) {
+            throw new InternalAuthenticationServiceException("An unexpected error occurred: 500 - Internal Server Error.", ex);
         }
-        UserEntity user = userRepository.findByUsername(request.getUsername())
-                .orElseThrow(() -> new UserNotFoundException("User not found"));
-        String jwtToken = jwtService.generateToken(user);
-        return AuthenticationResponse.builder()
-                .token(jwtToken)
-                .build();
     }
+
 
 
 }
